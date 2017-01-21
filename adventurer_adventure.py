6abc37eb0e03c5efcbd9e6e66fa2e2c8212fa798
Attakaroni <Attakaroni@gmail.com>
2017-01-21 15:26:39 -0700

    Fundamentals
    Adds fundamental functions to the game

--- adventurer_adventure.py
+++ adventurer_adventure.py
@@ -3,3 +3,60 @@
 # Vitridax
 # Wyzat
 
+import pygame
+import sys
+import traceback
+import random
+
+pygame.init()
+
+WINDOW_SIZE = 768
+FPS = 64
+
+def update(game):
+    pass
+
+def handle_events(game):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game['mode'} = "Quit"
+
+    pressed = pygame.key.get_pressed()
+
+def render(screen, game):
+    pass
+
+def reset():
+    game = {'mode': "Run"}
+    return game
+
+def setup():
+    screen = pygame.display.setmode((WINDOW_SIZE, WINDOW_SIZE))
+    pygame.display.set_caption("Adventurer Adventure")
+    clock = pygame.time.Clock()
+    game = reset()
+    #play_music(game['music'])
+    return screen, clock, game
+
+def unsetup():
+    stop_music()
+
+def main():
+    screen, clock, game = setup()
+    while game['mode'] == "Run":
+        handle_events(game)
+        update(game)
+        render(screen, game)
+        clock.tick(FPS)
+    unsetup()
+
+try:
+    main()
+except Exception as e:
+    for frame in traceback.extract_tb(sys.exc_info()[2]):
+        fname, lineno, fn, text = frame
+        print("You screwed up in %s on line %d" % (fname, lineno))
+    print(sys.exc_info())
+finally:
+    pygame.quit()
+    sys.exit
